ab253ffad252af9f5cc7adccb88c68be9f3ff469
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-29 13:28:39 +0300

    create an image model.

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt 
 
 # Create your models here.
+class Image(models.Model):
+  '''
+  Image model to help create new instances of an image object
+  '''
+  name = models.CharField(max_length=30)
+  description = models.TextField()
+
+
