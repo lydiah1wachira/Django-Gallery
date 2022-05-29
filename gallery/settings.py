1f37df2bcc6bab1edb76ec760937064154ca7ad5
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-29 12:29:26 +0300

    fix errors in the pages

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap5'
+    'bootstrap5',
     'photos.apps.PhotosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
