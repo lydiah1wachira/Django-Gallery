583d8f1b7c781bef2339283e9aeffec26d6f0567
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-30 19:01:39 +0300

    make new migration after adding a category column to the image model,

--- photos/models.py
+++ photos/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 import datetime as dt 
-from photos.models import Category
+
 
 # Create your models here.
-
-class Image(models.Model):
-  '''
-  Image model to help create new instances of an image object
-  '''
-  name = models.CharField(max_length=30)
-  description = models.TextField()
-  pub_date = models.DateTimeField(auto_now_add=True)
-  category = models.ForeignKey(Category, on_delete=models.SET_NULL)
 
 
 class Category(models.Model):
@@ -22,5 +13,14 @@ class Category(models.Model):
 
   def __str__(self):
     return self.name 
+
+class Image(models.Model):
+  '''
+  Image model to help create new instances of an image object
+  '''
+  name = models.CharField(max_length=30)
+  description = models.TextField()
+  pub_date = models.DateTimeField(auto_now_add=True)
+  category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
 
