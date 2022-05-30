10b5b718448ea959a2393006a366609ee67beeb1
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-30 20:20:25 +0300

    install pillow

--- photos/models.py
+++ photos/models.py
@@ -14,6 +14,8 @@ class Category(models.Model):
   def __str__(self):
     return self.name 
 
+
+
 class Image(models.Model):
   '''
   Image model to help create new instances of an image object
@@ -22,5 +24,8 @@ class Image(models.Model):
   description = models.TextField()
   pub_date = models.DateTimeField(auto_now_add=True)
   category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+  def __str__(self):
+    return self.description
 
 
