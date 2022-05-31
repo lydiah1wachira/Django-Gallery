4a9153003340af157fca91931c6472dd9e239c43
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-31 04:49:56 +0300

    create a test class for the category model.

--- photos/tests.py
+++ photos/tests.py
@@ -1,8 +1,30 @@
 from django.test import TestCase
-from .models import Image,Category
+from .models import Image,Category, Location
 
 # Create your tests here.
-class ImageTest(TestCase):
+class CategoryTestClass(TestCase):
   def setUp(self):
+        self.genre = Category(name="Fashion")
+        self.genre.save_category()
+
+  def test_instance(self):
+      self.assertTrue(isinstance(self.genre, Category))
+
+  def test_save_method(self):
+      self.genre.save_category()
+      category = Category.objects.all()
+      self.assertTrue(len(category) > 0)
+
+  def test_delete_method(self):
+      self.genre.save_category()
+      self.genre.delete_category()
+      category = Category.objects.all()
+      self.assertTrue(len(category) == 0)
+
+  def test_update(self):
+      category = Category.get_category_id(self.genre.id)
+      category.update_category('nature')
+      category = Category.get_category_id(self.genre.id)
+      self.assertTrue(category.name == 'nature')
+
   
-    
