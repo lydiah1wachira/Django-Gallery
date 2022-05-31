2467f1a0b7830fe9ce21daa989f8e749163a543f
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-31 04:42:36 +0300

    add the image model methods.

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,6 @@
-from unicodedata import category
+
 from django.db import models
-import datetime as dt 
+
 
 
 # Create your models here.
@@ -67,6 +67,38 @@ class Image(models.Model):
   location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
 
   def __str__(self):
-    return self.description
+      return self.description
+
+  def save_image(self):
+      self.save()
+
+  def delete_image(self):
+      self.delete()
+
+  @classmethod
+  def update_image(cls,id, photo,name, description, location, category):
+      update = cls.objects.filter(id = id).update( image_name=name, description=description, location=location, category=category)
+      return update
+      
+  @classmethod
+  def get_images(cls):
+      images = cls.objects.all()
+      return images
+    
+  @classmethod
+  def search_by_category(cls, search_term):
+      images = cls.objects.filter(category__title__icontains=search_term)
+      return images
+    
+  @classmethod
+  def get_image_by_id(cls,id):
+      image = cls.objects.filter(id= id).all()
+      return image
+    
+  @classmethod
+  def filter_by_location(cls, img_location):
+      images_location = cls.objects.filter(location__id=img_location)
+      return images_location
+
 
 
